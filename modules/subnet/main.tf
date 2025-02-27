5718c8a0a8210726a92c0c4a781f53257d3fc730
Ansu-dev <ansu1007@naver.com>
2025-02-27 11:21:32 +0900

    ✨ Feat: public subnet - route table connect

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -6,6 +6,54 @@ resource "aws_subnet" "public-1" {
   availability_zone = "ap-northeast-2a"
 
   tags = {
-    Name = "public-subnet-1"
+    Name = "public-subnet-1" # 서브넷의 태그 설정
   }
 }
+
+# public subnet 2 생성
+resource "aws_subnet" "public-2" {
+  vpc_id = var.vpc_id
+  cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "ap-northeast-2c"
+
+  tags = {
+    Name = "public-subnet-2" # 서브넷의 태그 설정
+  }
+}
+
+# private subnet 1 생성
+resource "aws_subnet" "private-1" {
+  vpc_id = var.vpc_id
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "ap-northeast-2a"
+
+  tags = {
+    Name = "private-subnet-1"
+  }
+}
+
+# private subnet 2 생성
+resource "aws_subnet" "private-2" {
+  vpc_id = var.vpc_id
+  cidr_block = "10.0.4.0/24"
+  availability_zone = "ap-northeast-2c"
+
+  tags = {
+    Name = "private-subnet-2"
+  }
+}
+
+#### public subnet과 라우트 테이블 연결
+
+# public subnet1과 라우트 테이블에 연결
+resource "aws_route_table_association" "public-1" {
+  subnet_id = aws_subnet.public-1.id
+  route_table_id = var.route_table_id
+}
+# public subnet2과 라우트 테이블에 연결
+resource "aws_route_table_association" "public-2" {
+  subnet_id = aws_subnet.public-2.id
+  route_table_id = var.route_table_id
+}
+
--- modules/subnet/variables.tf
+++ modules/subnet/variables.tf
@@ -1,3 +1,4 @@
+# 외부의 output을 가져와 현재 모듈의 변수로 사용할수 있도록 주입
 variable "vpc_id" {}
 
 variable "route_table_id" {}
