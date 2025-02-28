c9c59a85eeb41c0fd7d458c7cc78a152069ebd47
Ansu-dev <ansu1007@naver.com>
2025-02-28 14:50:20 +0900

    ✨ Feat: create cloudwatch metric alarm

--- modules/auto-scaling/main.tf
+++ modules/auto-scaling/main.tf
@@ -16,3 +16,34 @@ resource "aws_autoscaling_group" "web1_asg" {
     value               = "web-asg"
   }
 }
+
+resource "aws_autoscaling_policy" "scale_up" {
+  name                   = "scale_up"
+  scaling_adjustment = 2 # 2개를 증가시킨다.
+  adjustment_type = "ChangeInCapacity"
+  cooldown = 300
+  autoscaling_group_name = aws_autoscaling_group.web1_asg.name
+}
+
+resource "aws_autoscaling_policy" "scale_down" {
+  name                   = "scale_down"
+  scaling_adjustment = -2 # 2개를 증가시킨다.
+  adjustment_type = "ChangeInCapacity"
+  cooldown = 300
+  autoscaling_group_name = aws_autoscaling_group.web1_asg.name
+}
+
+resource "aws_cloudwatch_metric_alarm" "high_cpu" {
+  alarm_name          = "high_cpu"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 2
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = 120
+  statistic           = "Average"
+  threshold           = 10
+  alarm_actions       = [aws_autoscaling_policy.scale_up.arn]
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.web1_asg.name
+  }
+}
